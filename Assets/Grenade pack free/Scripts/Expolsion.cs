8b0a4a6187657ebfa99567f43b4754bd2c049165
TolRiaz <34152412+TolRiaz@users.noreply.github.com>
2019-06-27 14:19:38 +0900

    bombed

--- Assets/Grenade pack free/Scripts/Expolsion.cs
+++ Assets/Grenade pack free/Scripts/Expolsion.cs
@@ -8,18 +8,23 @@ public class Expolsion : MonoBehaviour {
 	// Grenade explodes after a time delay.
 	private float Timer;
 	public float fuseTime;
-	Collider col = gameObject.GetComponent<BoxCollider>();
-
+	public float expTime = 0.1f;
+	public GameObject expPrefab;
 
 	void Start() {
 		Timer = 0;	// set the current time
 	}
 		
 	void Explode() {
-		
-		// var exp = GetComponent<ParticleSystem>();
-		// exp.Play();
-		Destroy(gameObject); //, exp.duration);
+		var col = gameObject.GetComponent<BoxCollider>();
+		col.size = new Vector3 (1.0f, 1.0f, 1.0f);
+		if (Timer >= fuseTime + expTime) {
+			GameObject exp = (GameObject)Instantiate (expPrefab, transform.position); 
+			var exp_component = exp.GetComponent<ParticleSystem>();
+			exp_component.Play ();
+			Destroy(gameObject, exp_component.duration);
+			Destroy (exp, exp_component.duration);
+		}
 	}
 
 	void Update() {
